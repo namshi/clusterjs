26205ceb1b111b20158a91ca4a1f9adf9670506f
odino <alessandro.nadalin@gmail.com>
2014-06-26 22:56:56 +0400

    removed debug

--- bin/cluster.js
+++ bin/cluster.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 var Cluster = require('../cluster.js'),
-    argv = require('yargs').argv,
-    debug = require('debug')('command');
+    argv = require('yargs').argv;
 
-var help = 'Cluster.js Command Help: \n\n' +
+var help = 'Cluster.js help: \n\n' +
            'Usage: clusterjs [path to app] [options] \n\n' +
            'Options:\n'+
            ' --workers\t Defines how many workers we should launch (Default: CPU count)\n' +
@@ -14,11 +13,13 @@ var workers = argv.workers  || require('os').cpus().length;
 var reloadSignal = argv.reloadon || 'SIGUSR2';
 var appName = argv['_'][0] || process.env.APP || null;
 
-debug('Cluster starting:: Appname->'+appName+'. Launching '+workers+' workers. Cluster will reload on: '+reloadSignal);
-
 if (appName === null || argv.help) {
     console.log(help);
     process.exit();
+} else {
+    console.log('Booting a cluster of ' + appName);
+    console.log('We are gonna be launching ' + workers + ' workers');
+    console.log('The cluster will reload on: ' + reloadSignal);
 }
 
 Cluster(appName, workers, reloadSignal);
