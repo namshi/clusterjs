5521c94f698b0af88896f3253dfbd9e3d3ce430b
odino <alessandro.nadalin@gmail.com>
2014-06-27 00:08:07 +0400

    ooooops, was running clusterjs only on the master process

--- bin/cluster.js
+++ bin/cluster.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 var cluster = require('cluster');
 
+var clusterjs = require('../cluster.js'),
+    argv = require('yargs').argv;
+
+var help = 'Cluster.js help: \n\n' +
+           'Usage: clusterjs [path to app] [options] \n\n' +
+           'Options:\n'+
+           ' --workers\t Defines how many workers we should launch (Default: CPU count)\n' +
+           ' --reloadon\t On Which signal will the cluster be reloaded (Default: SIGUSR2)' +
+            '\n\n';
+
+var workers = argv.workers  || require('os').cpus().length;
+var reloadSignal = argv.reloadon || 'SIGUSR2';
+var appName = argv['_'][0] || process.env.APP || null;
+
 if (cluster.isMaster) {
-    var clusterjs = require('../cluster.js'),
-        argv = require('yargs').argv;
-
-    var help = 'Cluster.js help: \n\n' +
-               'Usage: clusterjs [path to app] [options] \n\n' +
-               'Options:\n'+
-               ' --workers\t Defines how many workers we should launch (Default: CPU count)\n' +
-               ' --reloadon\t On Which signal will the cluster be reloaded (Default: SIGUSR2)' +
-                '\n\n';
-
-    var workers = argv.workers  || require('os').cpus().length;
-    var reloadSignal = argv.reloadon || 'SIGUSR2';
-    var appName = argv['_'][0] || process.env.APP || null;
-
     if (appName === null || argv.help) {
         console.log(help);
         process.exit();
@@ -24,6 +24,6 @@ if (cluster.isMaster) {
         console.log('We are gonna be launching ' + workers + ' workers');
         console.log('The cluster will reload on: ' + reloadSignal);
     }
-
-    clusterjs(appName, workers, reloadSignal);
 }
+
+clusterjs(appName, workers, reloadSignal);
