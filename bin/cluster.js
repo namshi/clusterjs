b0673a69be6f865c095621b14a124ab089658c5e
unLucio <unlucio@gmail.com>
2014-06-26 15:37:40 +0400

    Adding cluster.js base logic: * Commanind line * module lib

--- bin/cluster.js
+++ bin/cluster.js
@@ -3,12 +3,12 @@ var Cluster = require('../cluster.js'),
     argv = require('yargs').argv,
     debug = require('debug')('cluster.js-bin');
 
-console.log(argv._);
-
-
-var help = 'Cluster.js Command Help: \n\n\n Usage: clusterjs [appname] [options] \n\n Options: \n --workers\t Defines how many workers we should launch (Default: CPU count)\n --reloadon\t On Which Signal wohoiuld the cluster be rreloaded (Default: SIGUSR2)';
-
-
+var help = 'Cluster.js Command Help: \n\n' +
+           'Usage: clusterjs [path to app] [options] \n\n' +
+           'Options:\n'+
+           ' --workers\t Defines how many workers we should launch (Default: CPU count)\n' +
+           ' --reloadon\t On Which signal will the cluster be reloaded (Default: SIGUSR2)' +
+            '\n\n';
 
 var workers = argv.workers  || require('os').cpus().length;
 var reloadSignal = argv.reloadon || 'SIGUSR2';
@@ -16,8 +16,7 @@ var appName = argv['_'][0] || process.env.APP || null;
 
 debug('Cluster starting:: Appname->'+appName+'. Launching '+workers+' workers. Cluster will reload on: '+reloadSignal);
 
-if (appName === null) {
-    console.error('You need to supply an App name!');
+if (appName === null || argv.help) {
     console.log(help);
     process.exit();
 }
--- cluster.js
+++ cluster.js
@@ -1,19 +1,49 @@
-//require('newrelic');
 var cluster = require('cluster'),
-    debug = require('debug')('cluster.js-lib');
+    debugModule = require('debug')('cluster.js-module'),
+    debugLauncher = require('debug')('cluster.js-launcher'),
+    debugWorkerRestarter = require('debug')('cluster.js-workersRestarter');
 
-function launch (reloadSignal) {
+
+function restartWorkers(cluster, workers) {
+    ;
+    var workerKey = workers.shift();
+
+    debugWorkerRestarter('restarting worker: '+workerKey);
+
+    cluster.workers[workerKey].disconnect();
+    cluster.workers[workerKey].on("disconnect", function () {
+        debugWorkerRestarter("Shutdown complete for worker " + workerKey);
+    });
+    var newWorker = cluster.fork();
+    newWorker.on("online", function () {
+        debugWorkerRestarter("Replacement worker online.");
+        if (workers.length > 0) {
+            restartWorkers(cluster, workers);
+        }
+    });
+}
+
+
+function launch (appPath, noOfWorkers, reloadSignal) {
+
+    appPath = appPath || process.env.APP || null;
+    noOfWorkers = noOfWorkers || require('os').cpus().length;
+    reloadSignal = reloadSignal || "SIGUSR2";
+
+    if (appPath === null) {
+        throw new Error('Please provide a path to your app. You can either pass it as a parameter or as process.env.APP');
+    }
+
     if (cluster.isMaster) {
-        var cpuCount = require('os').cpus().length;
 
         // Create a worker for each CPU
-        for (var i = 0; i < cpuCount; i += 1) {
+        for (var i = 0; i < noOfWorkers; i += 1) {
             cluster.fork();
         }
 
         // Listen for dying workers
         cluster.on('exit', function (worker) {
-            console.log('Worker ' + worker.id + ' died :(');
+            debugLauncher('Worker ' + worker.id + ' died :(');
             // A suicide means we shutdown the worker on purpose
             // like in a deployment
             if (worker.suicide !== true) {
@@ -21,40 +51,27 @@ function launch (reloadSignal) {
             }
         });
 
-        process.on("SIGUSR2", function () {
-            console.log("SIGUSR2 received, this means we are deploying and want to reload the app");
+        process.on(reloadSignal, function () {
+            debugLauncher(reloadSignal + 'received, this means we are deploying and want to reload the app');
 
             // delete the cached module, so we can reload the app
-            delete require.cache[require.resolve("./app")];
+            delete require.cache[require.resolve(appPath)];
 
             // only reload one worker at a time
             // otherwise, we'll have a time when no request handlers are running
-            var i = 0;
             var workers = Object.keys(cluster.workers);
-
-            var f = function () {
-                if (i == workers.length) return;
-
-                console.log("Killing " + workers[i]);
-
-                cluster.workers[workers[i]].disconnect();
-                cluster.workers[workers[i]].on("disconnect", function () {
-                    console.log("Shutdown complete for worker " + i);
-                });
-                var newWorker = cluster.fork();
-                newWorker.on("listening", function () {
-                    console.log("Replacement worker online.");
-                    i++;
-                    f();
-                });
-            }
-            f();
+            restartWorkers(cluster, workers);
         });
     } else {
-        var app = require('./app');
-        console.log('Worker ' + cluster.worker.id + ' running!');
+        try {
+            var app = require(appPath);
+        } catch(error){}
+
+        debugLauncher('Worker ' + cluster.worker.id + ' running!');
     }
 }
-module.exports = function(appName, noOfWorkers, reloadSignal){
-    debug('doing things');
+module.exports = function(app, noOfWorkers, reloadSignal){
+
+    debugModule('Thank you for using Cluster.js!');
+    launch(app, noOfWorkers, reloadSignal);
 };
